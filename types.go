d77d39e56a26df17bb1db617aacc989aa81e9331
Mikhail Brel <m@brel.me>
2017-03-13 15:20:29 +0500

    fix json names

--- types.go
+++ types.go
@@ -1,33 +1,33 @@
 package telegram
 
 type Update struct {
-	UpdateId int     `json:update_id`
-	Message  Message `json:message`
+	UpdateId int     `json:"update_id"`
+	Message  Message `json:"message"`
 }
 
 type Message struct {
-	MessageId int    `json:message_id`
-	From      User   `json:from`
-	Date      int    `json:date`
-	Chat      Chat   `json:chat`
-	Text      string `json:text`
+	MessageId int    `json:"message_id"`
+	From      User   `json:"from"`
+	Date      int    `json:"date"`
+	Chat      Chat   `json:"chat"`
+	Text      string `json:"text"`
 }
 
 type User struct {
-	Id        int    `json:id`
-	FirstName string `json:first_name`
-	LastName  string `json:last_name`
-	UserName  string `json:username`
+	Id        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	UserName  string `json:"username"`
 }
 
 type Chat struct {
 	Id                          int    `json:"id"`
 	Type                        string `json:"chat_type"`
-	Title                       string `json:title`
-	Username                    string `json:username`
-	FirstName                   string `json:first_name`
-	LastName                    string `json:last_name`
-	AllMembersAreAdministrators bool   `json:all_members_are_administrators`
+	Title                       string `json:"title"`
+	Username                    string `json:"username"`
+	FirstName                   string `json:"first_name"`
+	LastName                    string `json:"last_name"`
+	AllMembersAreAdministrators bool   `json:"all_members_are_administrators"`
 }
 
 type ReplyMessage struct {
@@ -35,4 +35,17 @@ type ReplyMessage struct {
 	Text        string      `json:"text"`
 	ParseMode   string      `json:"parse_mode,omitempty"`
 	ReplyMarkup interface{} `json:"reply_markup,omitempty"`
+}
+
+type InlineKeyboardMarkup struct {
+	Keyboard        [][]KeyboardButton `json:"keyboard"`
+	ResizeKeyboard  bool               `json:"resize_keyboard"`
+	OneTimeKeyboard bool               `json:"one_time_keyboard"`
+	Selective       bool               `json:"selective"`
+}
+
+type KeyboardButton struct {
+	Text            string `json:"text"`
+	RequestContact  bool   `json:"request_contact"`
+	RequestLocation bool   `json:"request_location"`
 }
