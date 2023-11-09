c8f9cb6cfc6c817d56f16243508a29dcaa3df1bb
Mikhail Brel <m@brel.me>
2023-11-08 23:10:41 -0500

    Logs + cleanup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/minya/telegram
+
+go 1.21.1
+
+require github.com/minya/goutils v0.0.0-20180115114943-130dc18ce623
--- polling.go
+++ polling.go
@@ -10,22 +10,29 @@ type UpdateHandler func(upd *Update) error
 func StartPolling(api *Api, handle UpdateHandler, updateInterval time.Duration, offset int) error {
 	newOffset := offset
 	for {
+		log.Printf("Getting updates with offset %d\n", newOffset)
+
 		updates, err := api.GetUpdates(newOffset)
 		if err != nil {
+			log.Printf("Error while getting updates: %v\n", err)
 			return err
 		}
 		for _, upd := range updates {
 			err = handle(&upd)
 			if err != nil {
-				log.Printf("%#v\n", err)
-				api.SendMessage(ReplyMessage{
+				log.Printf("Error while handling update: %v\n", err)
+				api.SendMessage(ReplyMessage{ // TODO: allow to disable this
 					ChatId: upd.Message.From.Id,
-					Text:   "При обработке запроса прооизошла ошибка",
+					Text:   "Error while handling update",
 				})
 			}
+
 			log.Printf("Update received %#v\n", upd)
 			newOffset = upd.UpdateId + 1
 		}
+
+		log.Printf("Updates handled, new offset is %d\n", newOffset)
+		log.Printf("Sleeping for %v\n", updateInterval)
 		time.Sleep(updateInterval)
 	}
 }
